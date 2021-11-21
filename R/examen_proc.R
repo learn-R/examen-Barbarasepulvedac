be4caacf00667395d23833001eb54bf6ebd3deac
Barbarasepulvedac <bsepulvedacastillo@gmail.com>
2021-11-21 00:23:21 -0300

    AVANCE

---
+++ R/examen_proc.R
@@ -0,0 +1,63 @@
+##############################
+#Procesamiento de datos 
+#Universidad Alberto Hurtado 
+#Carrera de Sociología 
+#Opt. Análisis de datos estadísticos en R
+#Profesora: Valentina Andrade
+#Ayudantes: Dafne Jaime y Nicolás Godoy 
+#Alumna: Bárbara Sepúlveda 
+#22 de noviembre, 2021
+#############################
+
+#1. Carga de librerías
+pacman::p_load(tidyverse, sjPlot, haven, 
+               srvyr,
+               survey,
+               magrittr,
+               dyplr)
+#2. Carga de base de datos Termómetro Social
+TS4 <- read_dta("input/data/TS4.dta")
+
+#3. Selección de variables para nuevo DF
+
+datos_proc <- TS4 %>% 
+  select(sexo, edad, tipo_vivienda = b2, est_animo = c2, trat_salud = c3, problemas = c10)
+
+#Variables seleccionadas
+#sexo     #edad
+#b2: tipo de vivienda ¿Usted vive en casa o departamento?
+#c2: estado de animo luego de la pandemia COVID-19 ¿cómo evalúa su estado de ánimo actual?
+#c3: tratamiento medico durante pandemia ¿usted ha creído que necesita un tratamiento en salud mental con un profesional del área (con psicólogo, médico general o psiquiatra)?
+#c10: Por favor, indique con qué frecuencia usted puede hablar de sus problemas con su familia o amigos
+
+#4. Recodificacion variables 
+
+#EDAD en tramos
+
+datos_proc <- datos_proc %>% 
+  mutate(edad = as_factor(car::recode(edad, c("18:39='Jovenes';40:59='Adulto'; 60:96='Adulto mayor'"), as.factor = T, levels = c("Jovenes", "Adulto", "Adulto mayor"))))
+
+#Sexo, Tipo vivienda y Tratamiento salud a factor
+
+datos_proc <- datos_proc %>% 
+  mutate(sexo = as_factor(sexo),
+         tipo_vivienda = as_factor(tipo_vivienda),
+         trat_salud = as_factor(trat_salud))
+
+#Tratamiento salud 2 dummy 
+
+datos_proc <- datos_proc %>% 
+  mutate(trat_salud2 = if_else(trat_salud == "1. Sí", 1, 0))
+
+#Variable problemas
+
+datos_proc <- datos_proc %>% 
+  mutate(problemas = as_factor(case_when(problemas == 1 ~ "Nunca",
+                                         problemas == 2 ~ "Nunca",
+                                         problemas == 3 ~ "A veces",
+                                         problemas == 4 ~ "Siempre",
+                                         problemas == 5 ~ "Siempre",
+                                         FALSE ~ NA_character_)))
+
+#5. Guardamos el df procesado 
+saveRDS(datos_proc, file = "output/data/datos_proc.rds")
